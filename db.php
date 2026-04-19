7698719f2e221dd0d98a83d9c56a98137da598aa
greenwall03abad-code <greenwall03.abad@gmail.com>
2026-04-19 14:49:45 +0800

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,6 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
